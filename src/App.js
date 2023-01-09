a632f3d230be7d7d014d4f283cca87843d41731e
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-09 17:28:48 +0530

    Some new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import Navbar from './Component/Navbar';
+import News from './Component/News';
 
 export default class App extends Component {
+  a = "prabhu"
   render() {
     return (
-      <div>App Prabhue</div>
+      <>
+            <Navbar/>
+            <News pageSize={5} coutry="in" category="sports" /> 
+          
+      
+      </>
+
     )
-  }}
+  }
+}
---
+++ src/Component/Spaning.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Spaning() {
+    return (
+        <div className='text-center'>
+            <div className="spinner-border text-success" role="status">
+                <span className="sr-only"></span>
+            </div>
+        </div>
+    )
+}
